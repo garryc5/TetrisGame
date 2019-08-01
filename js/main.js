9407a5f97c1c153aa0022ee64fbf3bce16379cd0
gcarver <gcarver@gcarvers-MacBook-Pro.local>
2019-08-01 16:16:12 -0500

    got the base of the game down can now use a and d on the keyboard to move the falling block

--- js/main.js
+++ js/main.js
@@ -2,13 +2,19 @@
 * board 10x20 but add 4 buffer on the hight */
 
 /*----- constants -----*/ 
-
+const Colors =
+{
+    0 : 'rgb(75,79,79)',
+    1 : 'rgb(245,245,245)',
+    2 : 'rgb(245,245,245)'
+};
 /*----- app's state (variables) -----*/ 
-let level,score, boardArry;
+let level,score, boardArry, dropX, leftRight, leftRightBuffer;
+var timer;
 /*----- cached element references -----*/ 
 
 /*----- event listeners -----*/ 
-
+document.addEventListener('keypress',handleKeys);
 /*----- functions -----*/
 function intal()
 {
@@ -20,19 +26,31 @@ function intal()
     boardArry[x].fill(0,0,10);
     }
     score = 0;
-    displayBoard();
+    leftRight = 4;
+    dropX = 1;
+    leftRightBuffer=0;
+    startGame();
+    timer();
     render();
 }
 
 function render()
 {
-console.log('getting there');
+    for(let x=4;x<boardArry.length;x++)
+    {
+        for(let y=0;y<boardArry[x].length;y++)
+        {
+            grab(`${x}${y}`).style.backgroundColor = Colors[boardArry[x][y]];
+        }
+    }
 }
 
-function displayBoard()
+function startGame()
 {
+    grab('start-game').style.display='none';
+    grab('tetris').style.display='grid';
     let child;
-    for(let x=0;x<boardArry.length;x++)
+    for(let x=4;x<boardArry.length;x++)
     {
         for(let y=0;y<boardArry[x].length;y++)
         {
@@ -49,4 +67,25 @@ function grab(name)
 {
     return document.getElementById(name);
 }
-intal();
+function play()
+{
+if(dropX>22){return}
+else{
+boardArry[dropX][leftRight]=0;
+dropX++;
+leftRight+=leftRightBuffer;
+leftRightBuffer =0;
+boardArry[dropX][leftRight]=1;
+render();
+}
+}
+function timer()
+{
+ timer = setInterval(play,525-(level*25));
+}
+function handleKeys(e)
+{
+    console.log(e.code);
+if(e.code=="KeyA"){leftRight>0 ? leftRightBuffer=-1 : leftRightBuffer=0}
+if(e.code=="KeyD"){leftRight<9 ? leftRightBuffer=1 : leftRightBuffer=0}
+}
